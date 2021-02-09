657a4f7cb2618349cece68c8151c3e1307beec74
Maksym <maksym.tsapiv@ucu.edu.ua>
2021-02-09 16:34:11 +0200

    Comleting program

--- puzzle.py
+++ puzzle.py
@@ -3,6 +3,11 @@
 
 def horizontal_check(board: list):
     """
+    Horizontal check
+    >>> horizontal_check(['*  *', '**12'])
+    True
+    >>> horizontal_check(['****', '**11'])
+    False
     """
     for line in board:
         for digit in line:
@@ -13,6 +18,11 @@ def horizontal_check(board: list):
 
 def column_check(board: list):
     """
+    Column check
+    >>> column_check([' ***', ' *23', '*45*'])
+    True
+    >>> column_check(['1***', '1*2 ', '**5 '])
+    False
     """
     result = [[None] * len(board) for _ in range(len(board[0]))]
     for i in range(len(board)):
@@ -24,20 +34,35 @@ def column_check(board: list):
 
 
 
-
 def color_check(board: list):
     """
+    Check color
     """
-    pass
+    col_board = []
+    for i in range(5):
+        col_board.append(one_color(board, i, 4 - i))
+    return horizontal_check(col_board)
+    
 
 
+def one_color(board, i, j):
+    """
+    Returns one color
+    """
+    color = []
+    for k in range(5):
+        color.append(board[i + k][j])
+    for l in range(4):
+        color.append(board[i + 4][j + l + 1])
+    return color
+    
 
 
 def validate_board(board: list):
     """
-
+    Validate board
     """
-    pass
+    return horizontal_check(board) and color_check(board) and color_check(board)
 
 
 
@@ -49,12 +74,16 @@ if __name__ == '__main__':
     "* 4 1****",
     "     9 5 ",
     " 6  83  *",
-    "3   1  **",
+    "3      **",
     "  8  2***",
     "  2  ****"
     ]
     # print(horizontal_check(['***1***','***238 **','**2***',]))
     # print(column_check(['1***56',' ***23','*** **','  8** ','******','******']))
-    # print(column_check(board))
+    print(column_check(board))
+    # print(color_check(board))
+    # print(one_color(board, 0, 4))
+    # print(color_check(board))
+    # print(validate_board(board))
     # import doctest
     # doctest.testmod()
